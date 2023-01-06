f3cd8ea701169488f70c31cf0b8f222b9fc36c83
afuet <73286581+lllk140@users.noreply.github.com>
2023-01-06 19:11:34 +0800

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,25 @@
+@echo off
+
+cd %~dp0
+
+echo install requirements.txt
+
+pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+echo install app
+
+pyinstaller main.py -F
+
+cd dist
+
+move main.exe %~dp0
+
+cd %~dp0
+
+rd /s /q build
+
+rd /s /q dist
+
+del /f /s /q *.spec
+
+pause
